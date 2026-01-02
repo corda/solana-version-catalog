c897eccdc8c479035e6122361993a3311a41e38a
Shams Asari <shams.asari@r3.com>
2026-01-02 10:45:18 +0000

    Pointing to latest Sava port

--- solana-version-catalog/build.gradle.kts
+++ solana-version-catalog/build.gradle.kts
@@ -38,7 +38,7 @@ val savaJsonIterator = "21.0.11-j17-1"
 val bouncyCastle = "1.81"
 
 // https://central.sonatype.com/search?namespace=software.sava
-val sava = "25.0.2"
+val sava = "25.1.2-j17-2"
 val savaWeb2 = "25.0.0"
 val savaPrograms = "25.0.0"
 val savaAnchorPrograms = "25.1.4"
